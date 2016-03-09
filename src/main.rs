1e500b6b2cea22a2e584455151ddf7e3606d4403
Marcel Müller <neikos@neikos.email>
2016-03-09 12:27:01 +0100

    Add Travis config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![deny(missing_docs,
+        missing_debug_implementations, missing_copy_implementations,
+        trivial_casts, trivial_numeric_casts,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces, unused_qualifications)]
+
+//! Antikoerper is a simple and lightweight data aggregation and visualization tool
+
 extern crate rustc_serialize;
 extern crate toml;
 
