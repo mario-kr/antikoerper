31e104040f1e45228cfc964c0853fcef0656e91b
Mario Krehl <mario-krehl@gmx.de>
2018-11-02 18:40:46 +0100

    Revert "Remove PartialEq, Eq, PartialOrd, Ord implementations"
    This reverts commit 5c05623f19ff3a1500dd32d426de583e7cd0f1aa.

--- src/item.rs
+++ src/item.rs
@@ -66,7 +66,7 @@ pub enum ItemKind {
 }
 
 /// A single item, knowing when it is supposed to run next, what should be done and its key.
-#[derive(Debug, Clone, Deserialize)]
+#[derive(Debug, Clone, Eq, PartialEq, Deserialize)]
 pub struct Item {
     #[serde(skip, default = "next_time_default")]
     pub next_time: i64,
@@ -83,7 +83,7 @@ pub struct Item {
     pub mappers: Vec<Mapper>,
 }
 
-#[derive(Debug, Clone, Deserialize)]
+#[derive(Debug, Clone, Eq, PartialEq, PartialOrd, Deserialize)]
 pub enum Mapper {
     Regex {
         #[serde(with = "serde_regex")]
@@ -96,9 +96,28 @@ fn next_time_default() -> i64 {
     0
 }
 
+impl PartialOrd for Item {
+    fn partial_cmp(&self, other: &Self) -> Option<::std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Item {
+    fn cmp(&self, other: &Self) -> ::std::cmp::Ordering {
+        if self.next_time < other.next_time {
+            ::std::cmp::Ordering::Greater
+        } else if self.next_time == other.next_time {
+            ::std::cmp::Ordering::Equal
+        } else {
+            ::std::cmp::Ordering::Less
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::path::PathBuf;
+    use std::collections::BinaryHeap;
     use std::collections::BTreeMap;
 
     use toml;
@@ -107,14 +126,13 @@ mod tests {
 
     #[test]
     fn items_ordered_by_smallest_time_first() {
-        let mut heap = Vec::new();
+        let mut heap = BinaryHeap::new();
         heap.push(Item {
             next_time: 5,
             interval: 5,
             env: BTreeMap::new(),
             key: String::from("tests.one"),
             kind: ItemKind::File{ path: PathBuf::from("/dev/null") },
-            mappers: vec![],
         });
         heap.push(Item {
             next_time: 3,
@@ -122,7 +140,6 @@ mod tests {
             env: BTreeMap::new(),
             key: String::from("tests.two"),
             kind: ItemKind::File{ path: PathBuf::from("/dev/null") },
-            mappers: vec![],
         });
 
         if let Some(item) = heap.pop() {
